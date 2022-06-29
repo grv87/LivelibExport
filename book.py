6143fd4d9a60ddc7de99d2a5e8ca12930c437aac
Basil Peace <grv87@yandex.ru>
2022-06-29 14:36:15 +0300

    fix: don't append domain name if it's absolute already

--- book.py
+++ book.py
@@ -1,9 +1,11 @@
+from urllib.parse import urljoin
+
 class Book:
 	def __init__(this, link, rating, date):
 		this.link = link
 		this.rating = rating
 		this.id = link[link.rfind("/")+1:]
-		this.full_link = "https://livelib.ru" + link
+		this.full_link = urljoin("https://livelib.ru", link)
 		this.date = date
 		this.name = None
 		this.ISBN = None
